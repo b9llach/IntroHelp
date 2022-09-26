63c3537cbab0694dc16ce063d06f393be2d63906
b9llach <61808505+b9llach@users.noreply.github.com>
2022-09-26 11:56:23 -0400

    Add files via upload

---
+++ Unit 4/COP 4-2 Debugging.cs
@@ -0,0 +1,25 @@
+﻿using static System.Console; // allows us to use Write without having to do Console.Write, puts us in console namespace
+
+int num1, num2, num3; // declare 3 vars that are ints
+
+Write("Enter first number >> "); // print to screen
+num1 = Convert.ToInt32(ReadLine()); // takes in user input, convert it an int, set it to num1
+Write("Enter second number >> "); // print to screen
+num2 = Convert.ToInt32(ReadLine()); // takes in user input, convert it an int, set it to num2
+Write("Enter third number >> "); // print to screen
+num3 = Convert.ToInt32(ReadLine()); // takes in user input, convert it an int, set it to num3
+
+if (num1 <= num2 && num2 <= num3) // checks if num1 is less then or equal to num2 AND (&& means and) num2 is less then or equal to num3
+{
+    WriteLine("Numbers are in sequential order");  // prints to screen
+}
+else if (num1 >= num2 && num2 >= num3) // checks if num1 is greater then or equal to num2 AND (&& means and) num2 is greater then or equal to num3
+{
+    WriteLine("Numbers are in reverse order"); // prints to screen
+}
+else // neither condition has been met
+{
+    WriteLine("Numbers are in neither sequential nor reverse order"); // print to screen
+}
+
+
---
+++ Unit 4/COP 4-4 Debugging.cs
@@ -0,0 +1,34 @@
+﻿using static System.Console; // puts us in console namespace
+
+double sales, commission; // unassigned double variable
+// const variables value's can not be changed after first assignment
+const int LOWSALES = 1000;
+const int MEDSALES = 5000;
+const int HIGHSALES = 10000;
+const double LOWPCT = 0.05;
+const double MEDPCT = 0.07;
+const int BONUS1 = 1000;
+const int BONUS2 = 1500;
+WriteLine("What was the sales amount? "); // print to screen
+sales = Convert.ToDouble(ReadLine()); // converts user input to a double, and set sales equal to it
+commission = LOWPCT * Math.Min(sales, LOWSALES); // sets commission to whichever value is lower
+if (sales <= LOWSALES) // if sales is less then or equal to lowsales
+{
+    commission += (sales - LOWSALES) * LOWPCT; // += is a short cut to add the variable onto itself. for example x+=1 is just x = x + 1
+    // so commission += (sales - LOWSALES) * LOWPCT is just commission = commission + (sales - LOWSALES) * LOWPCT
+}
+else if (sales <= MEDSALES) // if sales is less then or equal to medsales
+{
+    commission += ((sales - LOWSALES) * MEDPCT); // again using += shortcut
+}
+
+else if (sales <= HIGHSALES) // if sales is less then or equal to highsales
+{
+    commission += ((sales - LOWSALES) * MEDPCT) + BONUS1; 
+}
+else // if no other condition is being met, we use this
+{
+    commission += ((sales - LOWSALES) * MEDPCT) + BONUS1 + BONUS2;
+}
+
+WriteLine($"Sales: {sales.ToString("C")}\nCommission: {commission.ToString("C")}"); // prints to screen
