846aeaafd83e24b64026e0a4c211c32a2301ee2d
b9llach <61808505+b9llach@users.noreply.github.com>
2022-10-15 12:13:41 -0400

    Add files via upload

--- Unit 4/COP 4-2 Debugging.cs
+++ Unit 4/COP 4-2 Debugging.cs
@@ -1,25 +1,40 @@
-﻿using static System.Console; // allows us to use Write without having to do Console.Write, puts us in console namespace
-
-int num1, num2, num3; // declare 3 vars that are ints
-
-Write("Enter first number >> "); // print to screen
-num1 = Convert.ToInt32(ReadLine()); // takes in user input, convert it an int, set it to num1
-Write("Enter second number >> "); // print to screen
-num2 = Convert.ToInt32(ReadLine()); // takes in user input, convert it an int, set it to num2
-Write("Enter third number >> "); // print to screen
-num3 = Convert.ToInt32(ReadLine()); // takes in user input, convert it an int, set it to num3
-
-if (num1 <= num2 && num2 <= num3) // checks if num1 is less then or equal to num2 AND (&& means and) num2 is less then or equal to num3
+﻿// Program asks user to enter three integers
+// Program displays a message indicating whether the numbers
+// are in sequential order,
+// in reverse order
+// or in neither order
+using System;
+using static System.Console;
+class DebugFour2
 {
-    WriteLine("Numbers are in sequential order");  // prints to screen
-}
-else if (num1 >= num2 && num2 >= num3) // checks if num1 is greater then or equal to num2 AND (&& means and) num2 is greater then or equal to num3
-{
-    WriteLine("Numbers are in reverse order"); // prints to screen
-}
-else // neither condition has been met
-{
-    WriteLine("Numbers are in neither sequential nor reverse order"); // print to screen
-}
+    static void Main()
+    {
+        int num1, num2, num3; // create 3 variables of type int
+        Write("Enter first number >> "); // prints to screen
+        num1 = Convert.ToInt32(ReadLine());
+        Write("Enter second number >> "); // prints to screen
+        num2 = Convert.ToInt32(ReadLine());
+        Write("Enter third number >> "); // prints to screen
+        num3 = Convert.ToInt32(ReadLine());
 
-
+        if (num1 <= num2 && num2 <= num3) // if num1 is less then or equal to num2, and num2 is less then or equal to num3
+        {
+            // in this case, if we entered 1, 2, 3, we would hit this part of the if statement
+            // we can also enter 2, 2, 3, since 2 is less then or equal to 2, and 3 is greater then 2
+            // entering 2, 2, 2 also works here because all of the number can be equal due to <= and not just <
+            WriteLine("Numbers are in sequential order");
+        }
+        else if (num1 >= num2 && num2 >= num3)
+        {
+            // in this case, if we entered 3, 2, 1 we would hit this part of the if statement
+            // we can also enter 3, 2, 2 since 3 is greater then or equal to 2, and 2 is less then or equal to 2
+            WriteLine("Numbers are in reverse order");
+        }
+        else
+        {
+            // if neither of those conditions are met, we hit this part
+            // if we entered 4, 6, 5, we would get this far, since neither of those other conditions are met
+            WriteLine("Numbers are in neither sequential nor reverse order");
+        }
+    }
+}
