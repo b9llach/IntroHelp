01a1bd29c727449328ab3616759db033cc9a570b
b9llach <61808505+b9llach@users.noreply.github.com>
2022-10-16 15:45:44 -0400

    Update COP 4-2 Case Problem.cs

--- Unit 4/COP 4-2 Case Problem.cs
+++ Unit 4/COP 4-2 Case Problem.cs
@@ -9,21 +9,21 @@ class MarshallsRevenue
         int EXTERIOR_PRICE = 750; // set exterior price
         int numInterior, numExterior, revenueInterior, revenueExterior, total, month;
         bool isInteriorGreater;
-        Write("Enter month >> ");
-        month = Convert.ToInt32(ReadLine());
-        Write("Enter number of interior murals scheduled >> ");
-        numInterior = Convert.ToInt32(ReadLine());
-        Write("Enter number of exterior murals scheduled >> ");
-        numExterior = Convert.ToInt32(ReadLine());
-        if (new[] { 12, 1, 2 }.Contains(month)) // checks if month is in the array which includes 12, 1, 2
+        Write("Enter month >> "); // prints to screen
+        month = Convert.ToInt32(ReadLine()); // converts user input to int
+        Write("Enter number of interior murals scheduled >> "); // prints to screen
+        numInterior = Convert.ToInt32(ReadLine()); // converts user input to int
+        Write("Enter number of exterior murals scheduled >> "); // prints to screen
+        numExterior = Convert.ToInt32(ReadLine()); // converts user input to int
+        if (month == 12 || month == 1 || month == 2) // checks if month is 12, 1, 2
         {
             numExterior = 0;
         }
-        if (new[] { 4, 5, 9, 10 }.Contains(month)) // checks if month is in the array which includes 4, 5, 9, 10
+        if (month == 4 || month == 5 || month == 9 || month == 10) // checks if month is 4, 5, 9, 10
         {
             EXTERIOR_PRICE = 699;
         }
-        if (new[] { 7, 8 }.Contains(month)) // checks if month is in the array which includes 7 and 8
+        if (month == 7 || month == 8) // checks if month is 7 or 8
         {
             INTERIOR_PRICE = 450;
         }
